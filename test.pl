160ca9841a8f212da27b9b265ae62238fe858d08
ramprabhu <ramprabhu.mca@gmail.com>
2021-11-02 17:46:53 +0530

    docker comment updated

--- test.pl
+++ test.pl
@@ -1,10 +1,5 @@
 print("Trigger hook with git-hub \n");
 
-print("Welcome main \n");
-print("Hello World \n");
-
-print("welcome test branch \n");
-
 print("Integrated with Jenkins successfully!!! \n");
 
 print("Docker integration\n");
